80c5394437e523d8c708d1b0624fcd43f3ec2261
Kissy1999 <35816345+Kissy1999@users.noreply.github.com>
2018-02-10 17:51:51 -0500

    Initial commit

---
+++ Lab project/Assets/scripts/Player_controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_controller : MonoBehaviour {
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+        // debug.log ("h=" + moveHorizontal "v=" + moveVertical);
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        Rigidbody2D rbody = this.gameObject.GetComponent<Rigidbody2D>();
+        rbody.velocity = movement * speed;
+    }
+}
